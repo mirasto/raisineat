efe14db0d2412154d0d5abd6ef26433c0a62d5be
mirasto <you@example.com>
2026-09-07 12:59:09 +0300

    feat: splash screen

--- src/navigators/authentication/index.tsx
+++ src/navigators/authentication/index.tsx
@@ -1,13 +1,18 @@
-import Login from '../../features/authentication/screens/Login/Login';
 import React, {FC} from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Splash from '../../features/authentication/screens/Splash';
+import Login from '../../features/authentication/screens/Login/Login';
 
 const Stack = createNativeStackNavigator<AuthStackParamList>();
 
 const AuthenticationNavigation: FC = () => {
   return (
-    <Stack.Navigator screenOptions={{headerShown: false}}>
-      <Stack.Screen name="Login" component={Login}/>
+    <Stack.Navigator
+      initialRouteName="Splash"
+      screenOptions={{headerShown: false}}
+    >
+      <Stack.Screen name="Splash" component={Splash} />
+      <Stack.Screen name="Login" component={Login} />
     </Stack.Navigator>
   );
 };
